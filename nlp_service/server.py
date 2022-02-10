2c86892843bf180345fff911765a1558143453a7
Mirko Lenz <info@mirko-lenz.de>
2022-02-10 11:44:20 +0100

    Make server available via cli

--- nlp_service/server.py
+++ nlp_service/server.py
@@ -326,6 +326,37 @@ class NlpService(nlp_pb2_grpc.NlpServiceServicer):
         return res
 
 
+app = typer.Typer()
+
+
+def add_services(server: grpc.Server):
+    """Add the services to the grpc server."""
+
+    nlp_pb2_grpc.add_NlpServiceServicer_to_server(NlpService(), server)
+    # topic_modeling_pb2_grpc.add_TopicModelingServiceServicer_to_server(
+    #     TopicModelingService(), server
+    # )
+
+
+@app.command()
+def main(host: str, port: int, processes: int = 1):
+    """Main entry point for the server."""
+
+    arg_services_helper.serve(
+        host,
+        port,
+        add_services,
+        processes=processes,
+        reflection_services=[
+            arg_services_helper.full_service_name(nlp_pb2, "NlpService"),
+        ],
+    )
+
+
+if __name__ == "__main__":
+    app()
+
+
 # [
 #     "",
 #     "IS_ALPHA",
