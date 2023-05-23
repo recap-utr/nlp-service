b5c8c4f30cc524916f64f52e791b132e78c635ca
Mirko Lenz <mirko@mirkolenz.com>
2023-05-23 22:46:20 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+# https://devenv.sh/reference/options/
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs@{ nixpkgs, flake-parts, devenv, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ devenv.flakeModule ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { pkgs, ... }: {
+        devenv.shells.default = {
+          languages.python = {
+            enable = true;
+            package = pkgs.python311;
+            poetry = {
+              enable = true;
+              activate.enable = true;
+              install.enable = true;
+            };
+          };
+        };
+      };
+    };
+}
