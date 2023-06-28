594a1cca06ed4e23b3dd3a11e2a93d9535b7780d
Mirko Lenz <mirko@mirkolenz.com>
2023-06-28 23:03:47 +0200

    build: add nix flake package

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs @ {
     nixpkgs,
     flake-parts,
     systems,
+    poetry2nix,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -15,21 +20,47 @@
       perSystem = {
         pkgs,
         lib,
+        system,
+        self',
         ...
-      }: {
-        devShells.default = let
-          python = pkgs.python311;
-          poetry = pkgs.poetry;
-        in
-          pkgs.mkShell {
-            packages = [poetry python];
-            POETRY_VIRTUALENVS_IN_PROJECT = true;
-            LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.stdenv.cc.cc];
-            shellHook = ''
-              ${lib.getExe poetry} env use ${lib.getExe python}
-              ${lib.getExe poetry} install --all-extras --no-root
-            '';
+      }: let
+        python = pkgs.python310;
+        poetry = pkgs.poetry;
+      in {
+        # _module.args.pkgs = import nixpkgs {
+        #   inherit system;
+        #   config.allowUnfree = true;
+        #   config.cudaSupport = true;
+        # };
+        packages = {
+          default = poetry2nix.legacyPackages.${system}.mkPoetryApplication {
+            inherit python;
+            projectDir = ./.;
+            preferWheels = true;
+            # overrides = poetry2nix.legacyPackages.${system}.overrides.withDefaults (self: super: {});
           };
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "nlp-service";
+            tag = "latest";
+            created = "now";
+            config = {
+              entrypoint = [(lib.getExe self'.packages.default)];
+              cmd = ["--help"];
+            };
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          packages = [poetry python];
+          POETRY_VIRTUALENVS_IN_PROJECT = true;
+          LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+            stdenv.cc.cc
+            zlib
+          ]);
+          shellHook = ''
+            ${lib.getExe poetry} env use ${lib.getExe python}
+            ${lib.getExe poetry} install --all-extras --no-root
+          '';
+        };
       };
     };
 }
