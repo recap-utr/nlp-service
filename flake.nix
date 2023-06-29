45070d7a8adae7b312354f35272e3aa84c5c2ad3
Mirko Lenz <mirko@mirkolenz.com>
2023-06-29 10:07:16 +0200

    fix: add ld_preload to nix flake

--- flake.nix
+++ flake.nix
@@ -27,20 +27,12 @@
         python = pkgs.python310;
         poetry = pkgs.poetry;
       in {
-        # _module.args.pkgs = import nixpkgs {
-        #   inherit system;
-        #   config.allowUnfree = true;
-        #   config.cudaSupport = true;
-        # };
         packages = {
           default = poetry2nix.legacyPackages.${system}.mkPoetryApplication {
             inherit python;
             projectDir = ./.;
             preferWheels = true;
             overrides = poetry2nix.legacyPackages.${system}.overrides.withDefaults (self: super: {
-              ml-dtypes = super.ml-dtypes.overridePythonAttrs (old: {
-                buildInputs = (old.buildInputs or []) ++ [super.setuptools super.pybind11];
-              });
               sentence-transformers = super.sentence-transformers.overridePythonAttrs (old: {
                 buildInputs = (old.buildInputs or []) ++ [super.setuptools];
               });
@@ -59,10 +51,8 @@
         devShells.default = pkgs.mkShell {
           packages = [poetry python];
           POETRY_VIRTUALENVS_IN_PROJECT = true;
-          LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
-            stdenv.cc.cc
-            zlib
-          ]);
+          LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [stdenv.cc.cc zlib]);
+          LD_PRELOAD = "/usr/lib/${system}-gnu/libcuda.so";
           shellHook = ''
             ${lib.getExe poetry} env use ${lib.getExe python}
             ${lib.getExe poetry} install --all-extras --no-root
