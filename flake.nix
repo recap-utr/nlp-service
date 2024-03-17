f5ee8b0fabf8266779b87ef9304a07901e2bfe2f
Mirko Lenz <mirko@mirkolenz.com>
2024-03-17 18:17:18 +0100

    build: update flake.nix

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
         legacyPackages.dockerManifest = flocken.legacyPackages.${system}.mkDockerManifest {
           github = {
             enable = true;
-            token = builtins.getEnv "GH_TOKEN";
+            token = "$GH_TOKEN";
           };
           version = builtins.getEnv "VERSION";
           images = with self.packages; [x86_64-linux.docker];
